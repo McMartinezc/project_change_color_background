7aa48af6dacb33e9a5077aad5d84ce62e682c130
McMartinezc <psyence_mc@hotmail.com>
2023-10-18 13:59:36 +0200

    color aplicado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+function changeColors(){
+    const random = Math.floor(Math.random() * colors.length);
+    document.querySelector('body').style.background = colors[random];
 
+}
